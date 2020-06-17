6840b5779240971fc714962a94c167ca35d5fef3
amal-sadan <you@yourdomain>
2020-06-17 14:30:22 -0700

    LAB 2

---
+++ bash/arithmetic-demo.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# this script demonstrates doing arithmetic
+
+# Task 1: Remove the assignments of numbers to the first and second number variables. Use one or more read commands to get 3 numbers from the user.
+# Task 2: Change the output to only show:
+#    the sum of the 3 numbers with a label
+#    the product of the 3 numbers with a label
+
+# Asking user to enter 3 random numbers
+echo "Enter the first number ::"
+read firstnumber
+
+echo "Enter the second number ::"
+read secondnumber
+
+echo "Enter the third number  ::"
+read thirdnumber
+
+#finding the sum and product of 3 numbers
+
+sum=$((firstnumber + secondnumber + thirdnumber))
+product=$((firstnumber * secondnumber * thirdnumber))
+
+#displaying the sum and product
+echo "sum = $sum"
+echo "product = $product"
